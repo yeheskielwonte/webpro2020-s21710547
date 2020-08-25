2bfba8031947647f89bfb394ce04f35f1333ab3a
owenyeheskiel <yeheskielwonte05@gmail.com>
2020-08-25 13:33:35 +0700

    Variable,Tipe data and Operator

---
+++ pertemuan_03/index.js
@@ -0,0 +1,38 @@
+/*
+    1. Variable and tipe
+    2. Operator
+    3. Array
+    4. Object
+    5. Fungsi
+    6. JavaScript Conditional
+    7. JavaScript Loop
+*/
+//=====================================================
+
+/*
+    1. Variable and tipe
+*/
+
+const firstName = "John";
+let age = 20;
+age = 21;
+const isMarried = true;
+
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof isMarried);
+
+
+const info =
+    "My name is " + firstName + ", i'm "+ age + " years old." + isMarried;
+
+console.log(typeof info);
+
+/*
+    2. Operator
+*/
+
+
+
+
+
