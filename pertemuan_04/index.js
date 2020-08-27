f0a3d4a80eeead13cb21b6e3a8d1fc1c297b4363
owenyeheskiel <yeheskielwonte05@gmail.com>
2020-08-27 13:11:20 +0700

    Function

---
+++ pertemuan_04/index.js
@@ -0,0 +1,20 @@
+/*
+    1. function
+*/
+
+function penjumlahanDuaBujurSangkar(a,b) {
+    return a*a+b*b;
+}
+console.log(penjumlahanDuaBujurSangkar(8,3));
+console.log(penjumlahanDuaBujurSangkar(9,4));
+
+
+function tambah(a,b){
+    return a+b;
+}
+
+function kali(a,b){
+    return a*b;
+}
+console.log(tambah(kali(4,8),kali(8,3)));
+
