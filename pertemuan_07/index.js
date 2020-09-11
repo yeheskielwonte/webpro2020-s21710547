af8c78bfd57db6976835f249bfa809af439b28f2
owenyeheskiel <yeheskielwonte05@gmail.com>
2020-09-11 14:03:54 +0800

    Tugas add pertemuan_07 Javascript conditional dan Javascript Loop

---
+++ pertemuan_07/index.js
@@ -0,0 +1,98 @@
+/*
+    1. Javascript Conditional
+*/
+
+//if else
+// let nilaiAndi = 79;
+
+// if (nilaiAndi > 80) {
+//   console.log("SANGAT MEMUASKAN");
+// } else if (nilaiAndi >= 60 && nilaiAndi <= 80) {
+//   console.log("MEMUASKAN");
+// } else {
+//   console.log("JANGAN MENYERAH, COBA LAGI!");
+// }
+
+//switch case
+// let seragamSekolah;
+// let hari = "selasa";
+
+// switch (hari) {
+//   case "senin":
+//     seragamSekolah = "kemeja putih bawahan merah";
+//     break;
+//   case "selasa":
+//     seragamSekolah = "kemeja hijau bawahan hitam";
+//     break;
+//   case "rabu":
+//     seragamSekolah = "kemeja putih bawahan putih";
+//     break;
+//   case "kamis":
+//     seragamSekolah = "kemeja batik merah bawahan hitam";
+//     break;
+//   case "jumat":
+//     seragamSekolah = "baju pramuka";
+//     break;
+//   default:
+//     seragamSekolah = "baju bebas";
+// }
+
+// console.log(seragamSekolah);
+
+
+
+
+/*
+    2.Javascript Loop
+*/
+
+//for loop
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+//   }  
+
+
+//for/in loop
+// const buku = {
+//     judul: "Harry Potter and The Philosopher's Stone",
+//     pengarang: "J. K. Rowling",
+//     tahun: 1997
+//  };
+//  for (x in buku) {
+//    console.log(x, ':', buku[x]);
+//  }
+ 
+
+//for/of loop
+// const buku = ["Game of Thrones: A Song of Ice and Fire",  
+//    "Harry Potter and The Philosopher's Stone", 
+//    "Lord of The Rings: The Fellowship of The Ring"
+// ];
+
+// for (x of buku) {
+//   console.log(x);
+// }
+
+
+//while loop 
+// let i = 1;
+
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+
+//do while loop 
+let i = 1;
+
+do {
+  console.log(i);
+  i++;
+} while (i <= 10);
+
+ 
+
+
+
+
+
