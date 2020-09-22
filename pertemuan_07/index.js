259457ad928d5833cedf6265787479205bd353f2
owenyeheskiel <yeheskielwonte05@gmail.com>
2020-09-22 13:42:42 +0800

    Mengenal scope dalam javascript dan template literal

--- pertemuan_07/index.js
+++ pertemuan_07/index.js
@@ -83,16 +83,12 @@
 // }
 
 //do while loop 
-let i = 1;
+// let i = 1;
 
-do {
-  console.log(i);
-  i++;
-} while (i <= 10);
-
- 
-
-
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 10);
 
 
 
