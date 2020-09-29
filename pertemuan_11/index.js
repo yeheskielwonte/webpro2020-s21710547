4805135121847463e4f2792d506d57b28de95fe4
owenyeheskiel <yeheskielwonte05@gmail.com>
2020-09-29 13:12:15 +0800

    add pertemuan 11 arrow function

---
+++ pertemuan_11/index.js
@@ -0,0 +1,43 @@
+//Var, Let, Const
+
+// let nama = 'stenly';
+// let nama = 'adam';
+
+// console.log(nama);
+
+//scope dalam javascript
+//1. Function scope (var)
+//1. Block scope
+
+// (function tes() {
+//     for (let i = 0; i < 10; i++) {
+//         console.log(i);;
+//     }
+// })(); //tanpa harus memanggil tes();
+// console.log(i);  //tidak bisa mengakses variable i diluar function dikarenakan i adalah Function scope
+
+// {
+//     let i;
+//     for(let i=0;i<10;i++){
+//         console.log(i);
+//     }
+// } //block scope
+
+// console.log(i);
+
+//Template Literal
+
+// const person = {
+//     firstName : 'owen',
+//     lastName : 'yeheskiel',
+//     age : 21,
+// };
+
+// console.log('Hallo nama saya '+person.firstName +' '+person.lastName+'. Umur saya adalah '+person.age);
+
+// console.log(`Hallo nama saya ${person.firstName} ${person.lastName}. Umur saya adalah ${person.age}`); //template literal
+
+
+
+
+
