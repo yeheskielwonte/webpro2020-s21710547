203f02035fb8949acff066838155e14f7c0d93ac
owenyeheskiel <yeheskielwonte05@gmail.com>
2020-10-13 13:46:33 +0800

    add pertemuan 16 Export and import

---
+++ pertemuan_16/hello.js
@@ -0,0 +1,13 @@
+// export const numbers = [1,2,3,4,5];
+
+
+// export const world = () =>{
+//     console.log(`Hello World`);
+// }
+
+
+const hello = (user)=>{
+    console.log(`hello, ${user}`);
+}
+
+export default hello;
---
+++ pertemuan_16/index.js
@@ -0,0 +1,9 @@
+// import {hello, world, numbers} from './hello.js';
+// import * as helper from './hello.js'
+
+import Func1 from './hello.js';
+
+Func1('John Doe');
+
+// helper.world();
+// console.log(helper.numbers);
