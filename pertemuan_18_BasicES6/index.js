260b203cf0b6bc2b16db75c3427817d9c7ca64a9
owenyeheskiel <yeheskielwonte05@gmail.com>
2020-11-03 12:55:19 +0800

    Promise

--- pertemuan_18_BasicES6/index.js
+++ pertemuan_18_BasicES6/index.js
@@ -1,21 +1,48 @@
-const second = () =>{
-    const num = 10;
-    setTimeout(() =>{
-        console.log('Async 1');
-        console.log(num);
+// const second = () =>{
+//     const num = 10;
+//     setTimeout(() =>{
+//         console.log('Async 1');
+//         console.log(num);
+//         setTimeout(()=>{
+//             console.log('Async 2');
+//             setTimeout(()=>{
+//                 console.log('Async 3');
+//             },2000)
+//         },2000)
+//     },2000, num)
+// }
+
+// const first = () => {
+//     console.log('First');
+//     second();
+//     console.log('The End');
+// }
+// first();
+
+
+//Promise
+
+const getData = () =>{
+    return new Promise((resolve, reject) =>{
         setTimeout(()=>{
-            console.log('Async 2');
-            setTimeout(()=>{
-                console.log('Async 3');
-            },2000)
+            const error=false;
+            if (error) {
+                reject('Success!!!');
+            } else {
+                resolve(10);
+            }
         },2000)
-    },2000, num)
+    })
 }
 
-const first = () => {
-    console.log('First');
-    second();
-    console.log('The End');
+const getName = id =>{
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            resolve(`ID: ${id} Success`)
+        },2000)
+    })
 }
-
-first();
+getData()
+    .then(id =>getName(id))
+    .then(success => console.log(success))
+    .catch(error=>{console.log(error)})
